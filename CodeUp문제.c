217a4a463511cc010160a10eb8863865f08a2e22
허세진 <heosejin222@gmail.com>
2025-01-06 15:46:43 +0900

    3004 데이터 재정렬

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,14 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
+int count[500001]; 
 int main()
 {
     int n;
-    int a[1001];
-    scanf("%d",&n);
-    for(int i = 0;i<n;i++)
-        scanf("%d",&a[i]);
-    
-    for(int i = 1;i<=n;i++)
-    printf("%d ",a[n-i]);
-    
+    scanf("%d", &n);
+    int nums[50001]; 
+    int result[50001]; 
+    int max = 0;
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+        count[nums[i]] = 1; 
+        if (nums[i] > max) max = nums[i];
+    }
+
+    int rank = 0;
+    for (int i = 0; i <= max; i++) {
+        if (count[i] == 1) {
+            count[i] = rank; 
+            rank++;
+        }
+    }
+
+   
+    for (int i = 0; i < n; i++) {
+        result[i] = count[nums[i]]; 
+    }
+
+   
+    for (int i = 0; i < n; i++) {
+        printf("%d ", result[i]);
+    }
+
     return 0;
 }
