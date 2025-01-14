b03f7a91614b0cad6dc412ebcbc2b5288ed8082c
허세진 <heosejin222@gmail.com>
2025-01-14 16:00:23 +0900

    1734 welcome

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,36 +1,10 @@
-#include <stdio.h>
-int count[500001]; 
+#include<stdio.h>
 int main()
 {
-    int n;
-    scanf("%d", &n);
-    int nums[50001]; 
-    int result[50001]; 
-    int max = 0;
-
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &nums[i]);
-        count[nums[i]] = 1; 
-        if (nums[i] > max) max = nums[i];
-    }
-
-    int rank = 0;
-    for (int i = 0; i <= max; i++) {
-        if (count[i] == 1) {
-            count[i] = rank; 
-            rank++;
-        }
-    }
-
-   
-    for (int i = 0; i < n; i++) {
-        result[i] = count[nums[i]]; 
-    }
-
-   
-    for (int i = 0; i < n; i++) {
-        printf("%d ", result[i]);
-    }
-
+    
+    char s[10];
+    scanf("%s",s);
+    printf("welcome! ");
+    printf("%s",s);
     return 0;
 }
