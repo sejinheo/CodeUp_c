5fb6b8cf1fe09f0d782fd89ad7852d7a2fffa961
허세진 <heosejin222@gmail.com>
2025-01-23 22:44:12 +0900

    1289 가장 큰 운동장

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,22 +1,17 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-    char speaking[100];
-    char reverse[100];
-    
-    gets(speaking);
-    int len = strlen(speaking);
-    
-    for(int i = 0; i<len;i++)
-    {
-        reverse[i] = speaking[len-i-1];
-        
-        if(i==len-1)
-        {
-            reverse[len] = 0;
-        }
-    }
-    printf("%s",&reverse);
-    return 0;
+    int n[6];
+    for(int i=0;i<=5;i++)
+    scanf("%d",&n[i]);
+    int a,b,c;
+    a = n[0] * n[1];
+    b = n[2] * n[3];
+    c = n[4] * n[5];
+    if(b<=a && c<=a)
+    printf("%d",a);
+    else if(a<=b&&c<=b)
+    printf("%d",b);
+    else 
+    printf("%d",c);
 }
