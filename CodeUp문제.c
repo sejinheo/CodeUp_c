4bca6c027b619914dec68f35d6ba1aba74d971e8
허세진 <heosejin222@gmail.com>
2025-01-29 23:35:25 +0900

    1580 (함수작성) 원의 넓이

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,53 +1,13 @@
 #include <stdio.h>
-typedef struct
+float circle(int r)
 {
-    char name[10];
-    int rank;
-    int num;
-}Student;
-int main()
+    float sum;
+    sum = 3.14*r*r;
+    return sum;
+}
+main()
 {
-    int max = 0;
-    Student student[101];
-    Student temp;
-    int n, m;
-    scanf("%d", &n);
-    scanf("%d", &m);
-
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%s", student[i].name);
-        scanf("%d", &student[i].rank);
-        student[i].num = i;
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        max = i;
-        for (int j = i + 1; j < n; j++)
-        {
-            if (student[j].rank > student[max].rank)
-            {
-                temp = student[j];
-                student[j] = student[max];
-                student[max] = temp;
-            }
-            if (student[j].rank == student[max].rank)
-            {
-                if (student[j].num < student[max].num)
-                {
-                    temp = student[j];
-                    student[j] = student[max];
-                    student[max] = temp;
-                }
-
-            }
-        }
-    }
-
-    for (int i = 0; i < m; i++)
-    {
-        printf("%s\n", student[i].name);
-        
-    }
+    int r;
+    scanf("%d", &r);
+    printf("%.2f", circle(r));
 }
