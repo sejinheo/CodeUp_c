b36a4ca4ec3c0a03ff140a95f2f129e45109d43b
허세진 <heosejin222@gmail.com>
2025-02-03 22:23:01 +0900

    1854 재귀로 각 자리 수의 합 리턴하기

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,35 +1,16 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+
+int f(long long n)
 {
-    int map[12][12];
-    int obstacle = 0;
-    for (int i = 1; i <= 11; i++)
-    {
-        for (int j = 1; j <= 10; j++)
-        {
-            scanf("%d", &map[i][j]);
-        }
-    }
-    for (int j = 1; j<=10; j++)
-    {
-        if (map[11][j] == 0)
-            continue;
+    if (n == 0) 
+        return 0;
+    return (n % 10) + f(n / 10);
+}
 
-        for (int i = 10; i >= 1; i--)
-        {
-            if (map[i][j] != 0)
-            {
-                obstacle = map[i][j];
-                break;
-            }
-        }
-        if (obstacle == 0)
-            printf("%d safe\n", j);
-        else if (obstacle > 0)
-            printf("%d crash\n", j);
-        else if (obstacle < 0)
-            printf("%d fall\n", j);
-        obstacle = 0;
-    }
-
+int main() 
+{
+    long long n;
+    scanf("%lld", &n);
+    printf("%d\n", f(n)); 
+    return 0;
 }
