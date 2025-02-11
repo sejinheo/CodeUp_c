fa9e301ba457a97f588df364d84f887ffa1378ea
허세진 <heosejin222@gmail.com>
2025-02-11 23:25:32 +0900

    2624 n이하의 소수의 합 구하기

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,29 +1,37 @@
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d", &n);
-    int arr[1001];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
+#include <stdio.h>
 
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d: ", i+1);
-        for (int j = 0; j < n; j++)
+int prime( int a );
+
+int main( )
+{
+    int input;
+    int i;
+    int sum;
+
+        sum = 0;
+
+        
+        scanf( "%d", &input );
+        if( input==0 )      return 0;
+
+        for( i=2 ; i<=input ; i++ )
         {
-            if (i != j)
-            {
-                if (arr[i] < arr[j])
-                    printf("< ");
-                else if (arr[i] > arr[j])
-                    printf("> ");
-                else if (arr[i] == arr[j])
-                    printf("= ");
-           }
+            sum+=prime( i );
         }
-        printf("\n");
+
+        printf("%d",sum );
+    return 0;
+}
+int prime( int a )
+{
+    int j;
+
+    for(j=2 ; j<=a ; j++)
+    {
+        if(a%j == 0)
+        {
+            if(a == j) return a;
+            if(a != j) return 0;
+        }
     }
 }
