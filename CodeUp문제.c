41d482b4a30b68ed468794f9fff9f8ceea4d2e36
허세진 <heosejin222@gmail.com>
2025-02-15 23:51:31 +0900

    2605 캔디팡

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,37 +1,37 @@
-#include <stdio.h>
-
-int prime( int a );
-
-int main( )
+#include<stdio.h>
+int arr[7][7];
+int func(int i, int j)
 {
-    int input;
-    int i;
-    int sum;
-
-        sum = 0;
-
-        
-        scanf( "%d", &input );
-        if( input==0 )      return 0;
-
-        for( i=2 ; i<=input ; i++ )
-        {
-            sum+=prime( i );
-        }
-
-        printf("%d",sum );
+    int color = arr[i][j];
+    if(color == 0) 
     return 0;
-}
-int prime( int a )
-{
-    int j;
+    arr[i][j] = 0;
 
-    for(j=2 ; j<=a ; j++)
+    int cnt = 1;
+    if(color == arr[i-1][j] && i >0) cnt += func(i-1,j);
+    if(color == arr[i+1][j] && i <6) cnt += func(i+1,j);
+    if(color == arr[i][j-1] && j >0) cnt += func(i,j-1);
+    if(color == arr[i][j+1] && j <6) cnt += func(i,j+1);
+    return cnt;
+}
+int main()
+{
+  for(int i = 0;i<7;i++)
+  {
+  for(int j = 0; j< 7;j++)
+  {
+    scanf("%d",&arr[i][j]);
+  }
+  }
+  int cnt = 0;
+  for(int i = 0;i<7;i++)
+  {
+    for(int j = 0;j<7;j++)
     {
-        if(a%j == 0)
-        {
-            if(a == j) return a;
-            if(a != j) return 0;
-        }
+        if(func(i,j) >= 3)
+            cnt++;
     }
+  }
+  printf("%d",cnt);
+    return 0;
 }
