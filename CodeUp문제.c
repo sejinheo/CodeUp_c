7dda670128b8d2fc4ba807915dccc47671a79a17
허세진 <heosejin222@gmail.com>
2025-02-16 23:20:15 +0900

    1713 두 배수의 합과 차

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,37 +1,24 @@
 #include<stdio.h>
-int arr[7][7];
-int func(int i, int j)
-{
-    int color = arr[i][j];
-    if(color == 0) 
-    return 0;
-    arr[i][j] = 0;
 
-    int cnt = 1;
-    if(color == arr[i-1][j] && i >0) cnt += func(i-1,j);
-    if(color == arr[i+1][j] && i <6) cnt += func(i+1,j);
-    if(color == arr[i][j-1] && j >0) cnt += func(i,j-1);
-    if(color == arr[i][j+1] && j <6) cnt += func(i,j+1);
-    return cnt;
-}
 int main()
 {
-  for(int i = 0;i<7;i++)
-  {
-  for(int j = 0; j< 7;j++)
-  {
-    scanf("%d",&arr[i][j]);
-  }
-  }
-  int cnt = 0;
-  for(int i = 0;i<7;i++)
-  {
-    for(int j = 0;j<7;j++)
+    int a,b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    int sum = 0;
+    for(int i = a;i<=b;i++)
     {
-        if(func(i,j) >= 3)
-            cnt++;
+        if(i%3 == 0)
+         sum += i;
+        else if(i%4 == 0)
+         sum -= i;
+        
+       if(i % 4 == 0 && i % 3 == 0)
+       sum -=i;
+       
+       //printf("%d\n",sum);
+        
     }
-  }
-  printf("%d",cnt);
+    printf("%d",sum);
     return 0;
 }
