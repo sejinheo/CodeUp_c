b70849761341d06f2c3e8362f791aaef452accb8
허세진 <heosejin222@gmail.com>
2025-02-19 23:13:21 +0900

    1714 숫자 거꾸로 출력하기

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,24 +1,16 @@
-#include<stdio.h>
 
-int main()
+#include <stdio.h>
+ 
+int main() 
 {
-    int a,b;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    int sum = 0;
-    for(int i = a;i<=b;i++)
-    {
-        if(i%3 == 0)
-         sum += i;
-        else if(i%4 == 0)
-         sum -= i;
-        
-       if(i % 4 == 0 && i % 3 == 0)
-       sum -=i;
-       
-       //printf("%d\n",sum);
-        
-    }
-    printf("%d",sum);
+    int s;
+    scanf("%d", &s);
+    do{
+        printf("%d", s%10);
+        s /= 10;
+        if(s==0)
+            break;
+    }while(1);
+    
     return 0;
 }
