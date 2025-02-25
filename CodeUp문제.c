d84e4a11d299185336770326007fa7a631bca882
허세진 <heosejin222@gmail.com>
2025-02-25 22:57:08 +0900

    1521 인삼밭(택배 수령)

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,29 +1,24 @@
 #include<stdio.h>
-
-int f(int n) 
-{
-	int num = 0;
-
-	while (n != 0) 
-	{ 
-		if (n % 10 == 1)
-		num++;
-		n /= 10;
-	}
-	return num; 
-}
-
+ 
 int main()
 {
-    int a,b;
-    int sum=0;
-    scanf("%d",&a);
-    scanf("%d",&b);
-    for (int i = a; i <= b; i++) 
+    int k,n,i,j,a[51][51],cnt=0;
+    scanf("%d %d",&k,&n);
+    for(i=1; i<=k ; i++)
+        for(j=1 ; j<=k ; j++)
+        {
+            scanf("%d",&a[i][j]);
+            if(a[i][j]>=6) 
+                a[i][j]=-1;
+        }
+        
+    for(i=1 ; i<=k ; i++)
     {
-		sum += f(i);
-	}
-	printf("%d", sum);
-
-    return 0;
+        for(j=1 ; j<=k ; j++)
+        {
+            if(a[i][j]!=-1 && a[i][j]+n>=1 && a[i][j]+n<=5)
+            cnt++;
+        }
+    }
+    printf("%d",cnt);
 }
