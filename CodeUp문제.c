3e86a4b9975b6a035b51a6d6fec899a6b9d58ce2
허세진 <heosejin222@gmail.com>
2025-02-28 23:42:09 +0900

    4816 전자레인지

--- CodeUp문제.c
+++ CodeUp문제.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
- 
 int main()
 {
-    int k,n,i,j,a[51][51],cnt=0;
-    scanf("%d %d",&k,&n);
-    for(i=1; i<=k ; i++)
-        for(j=1 ; j<=k ; j++)
-        {
-            scanf("%d",&a[i][j]);
-            if(a[i][j]>=6) 
-                a[i][j]=-1;
-        }
-        
-    for(i=1 ; i<=k ; i++)
+    int t;
+    scanf("%d", &t);
+    int button[3] = {300,60,10};
+    int count[3] = { 0 };
+    if (t % 10 !=0)
     {
-        for(j=1 ; j<=k ; j++)
+        printf("-1");
+        return 0;
+    }
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (t >= button[i])
         {
-            if(a[i][j]!=-1 && a[i][j]+n>=1 && a[i][j]+n<=5)
-            cnt++;
+            count[i] = t / button[i];
+            t = t % button[i];
         }
     }
-    printf("%d",cnt);
+    for (int i = 0; i < 3; i++)
+    {
+
+        printf("%d", count[i]);
+        if (i + 1 != 3)
+            printf(" ");
+    }
 }
